15da827687bf369dd94a67a6c1574f37ae8a2e35
Bruno Santos <brunosps00@gmail.com>
2021-08-02 19:42:48 -0300

    Adicionado role ao withAuth

--- src/components/withAuth/index.tsx
+++ src/components/withAuth/index.tsx
@@ -3,31 +3,44 @@ import { useRouter } from 'next/router';
 
 
 import AuthState from '../../dtos/AuthState';
-import User from '../../dtos/User';
 import ApiData from '../../dtos/ApiData';
 import { getCredential } from '../../services/credentials';
+import Employee from '../../dtos/Employee';
+import { useToast } from '@chakra-ui/react';
 
-const withAuth = (Component: any) => {
+const withAuth = (Component: any, role = "") => {
   const Auth = (props: any) => {
     const router = useRouter();
-    const loggedEmployee: User = useSelector((state: AuthState) => state.auth.loggedEmployee);
+    const toast = useToast();
+    const loggedEmployee: Employee = useSelector((state: AuthState) => state.auth.loggedEmployee);
 
     const apiData: ApiData = getCredential();
 
     if (!loggedEmployee ||
-        !apiData ||
-        !apiData['access-token'] ||
-        apiData['access-token'] === ''
-      ) {
-        router.push({
-          pathname: '/Auth/Login',
-          query: {
-            callback: router.pathname
-          }
-        });
-      }
+      !apiData ||
+      !apiData['access-token'] ||
+      apiData['access-token'] === ''
+    ) {
+      router.push({
+        pathname: '/Auth/Login',
+        query: {
+          callback: router.pathname
+        }
+      });
+    }
 
-      return <Component {...props}/>;
+    if (role !== "" && loggedEmployee.occupation !== role) {
+      toast({
+        title: "Usuário sem acesso",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+
+      router.push("/");
+    }
+
+    return <Component {...props} />;
   }
 
   if (Component.getServerSideProps) {
